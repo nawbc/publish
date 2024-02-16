1850a7679b64c77d0ecaaf184e9660b02efd194f
nawbc <wanghan9423@outlook.com>
2024-02-16 19:58:56 +0800

    git ignore

--- packages/doc-editor/src/preset/PublishDocEditor.tsx
+++ packages/doc-editor/src/preset/PublishDocEditor.tsx
@@ -66,6 +66,25 @@ export const PublishDocEditor = function () {
               <DocEditor.Bold />
               <DocEditor.Italic />
               <DocEditor.Link />
+              <DocEditor.ColorPicker
+                colors={[
+                  '#25262b',
+                  '#868e96',
+                  '#fa5252',
+                  '#e64980',
+                  '#be4bdb',
+                  '#7950f2',
+                  '#4c6ef5',
+                  '#228be6',
+                  '#15aabf',
+                  '#12b886',
+                  '#40c057',
+                  '#82c91e',
+                  '#fab005',
+                  '#fd7e14',
+                ]}
+              />
+              <DocEditor.CodeBlock />
             </DocEditor.FnGroup>
           </BubbleMenu>
         )}
