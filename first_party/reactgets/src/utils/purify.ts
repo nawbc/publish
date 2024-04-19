f2f43e8d4e86f63fb6fe1a2dab01409f1766c29e
nawbc <wanghan9423@outlook.com>
2024-04-19 10:30:01 +0800

    tmp

--- first_party/reactgets/src/utils/purify.ts
+++ first_party/reactgets/src/utils/purify.ts
@@ -2,6 +2,13 @@ export type Purify<T extends Record<string, any>> = {
   [Key in keyof T]-?: T[Key] extends undefined ? never : T[Key];
 };
 
+/**
+ *
+ * Remove object top level useless keys;
+ *
+ * @param record
+ * @returns
+ */
 export function purify<T extends Record<string, any>>(record: T) {
   return Object.keys(record).reduce<Purify<T>>((acc, key: keyof T) => {
     if (record[key] !== undefined) {
--- packages/desktop/src/components/Fallback/Errors.ts
+++ packages/desktop/src/components/Fallback/Errors.ts
@@ -1,4 +1,3 @@
-// export const
 // Whoops
 
 interface ConfigurableErrorOptions {
