0889b2230b36ea3a991641072338a9b5dd182e67
nawbc <wanghan9423@outlook.com>
2024-06-12 15:31:46 +0800

    Bump yarn@4.2.2

--- packages/dev/src/webpack/postcss.config.cjs
+++ packages/dev/src/webpack/postcss.config.cjs
@@ -1,16 +1,31 @@
 /* eslint-env node */
 
 module.exports = {
-  plugins: {
-    'postcss-preset-mantine': {},
-    'postcss-simple-vars': {
-      variables: {
-        'mantine-breakpoint-xs': '36em',
-        'mantine-breakpoint-sm': '48em',
-        'mantine-breakpoint-md': '62em',
-        'mantine-breakpoint-lg': '75em',
-        'mantine-breakpoint-xl': '88em',
+  plugins: Object.assign(
+    {},
+    {
+      'postcss-preset-mantine': {},
+      'postcss-simple-vars': {
+        variables: {
+          'mantine-breakpoint-xs': '36em',
+          'mantine-breakpoint-sm': '48em',
+          'mantine-breakpoint-md': '62em',
+          'mantine-breakpoint-lg': '75em',
+          'mantine-breakpoint-xl': '88em',
+        },
       },
     },
-  },
+    process.env.NODE_ENV !== 'development' && {
+      'postcss-flexbugs-fixes': {},
+      'postcss-preset-env': {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    },
+  ),
 };
